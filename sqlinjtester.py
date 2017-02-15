9dc4cdddc07b5502322e93216c2ffab43e9bc26d
farcompen <farcompen71@gmail.com>
2017-02-15 15:38:48 +0300

    Add files via upload

---
+++ sqlinjtester.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env pyhton
+
+import sys
+import urllib
+import urllib.request
+
+print("\n ****** Developer: Faruk GÜNGÖR **** \n ******faruk.gungor@saglik.gov.tr ******\n")
+hedef_url=input("Lütfen hedef url'yi giriniz: ")
+istek=urllib.request.urlopen(hedef_url + "'1=1")
+body=istek.read()
+fbody=body.decode('utf-8')
+if "You have an error in your SQL syntax" in fbody:
+    print("Hedef sitede  SQL injection zaafiyeti var")
+else:
+    print("Hedef sitede zaafiyet bulunamadı")
