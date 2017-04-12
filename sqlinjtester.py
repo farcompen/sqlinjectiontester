11f2a9385511a46d94717ebe673857f88422c3da
farcompen <farcompen71@gmail.com>
2017-04-12 10:32:37 +0300

    Update sqlinjtester.py

--- sqlinjtester.py
+++ sqlinjtester.py
@@ -4,7 +4,7 @@ import sys
 import urllib
 import urllib.request
 
-print("\n ****** Developer: Faruk GÜNGÖR **** \n ******faruk.gungor@saglik.gov.tr ******\n")
+
 hedef_url=input("Lütfen hedef url'yi giriniz: ")
 istek=urllib.request.urlopen(hedef_url + "'1=1")
 body=istek.read()
